3441c9eb30ec4284b95146efab518f8a90cf3a96
Kai Mühlbauer <kmuehlbauer@wradlib.org>
2024-11-14 13:28:24 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,7 @@
 # -- Project information
 
 import pooch
+import requests
 
 project = 'Lumache'
 copyright = '2021, Graziella'
@@ -36,4 +37,23 @@ html_theme = 'sphinx_rtd_theme'
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
 
+output_file = open("output_file.nc", "w+b")
+kwargs.setdefault("stream", True)
+try:
+    response = requests.get(url, timeout=30, **kwargs)
+    response.raise_for_status()
+    content = response.iter_content(chunk_size=1024)
+    total = int(response.headers.get("content-length", 0))
+    
+    for chunk in content:
+        if chunk:
+            output_file.write(chunk)
+            output_file.flush()
+            if self.progressbar:
+                # Use the chunk size here because chunk may be much
+                # larger if the data are decompressed by requests after
+                # reading (happens with text files).
+                progress.update(1024)
+finally:
+    output_file.close()
 fname = pooch.retrieve("https://github.com/openradar/open-radar-data/raw/main/data/cfrad.20080604_002217_000_SPOL_v36_SUR.nc", "67821b6c2bb0f27b5de49dee636f36e6e5bbad95f1ee168cb2d1af48e98992fe")
